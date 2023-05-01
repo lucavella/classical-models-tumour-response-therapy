aa232a8606a20cc6104ea67646ce8aa556d2a990
vdlluca <luca.iacovella@vub.be>
2023-05-01 18:15:47 +0200

    added few classical models

--- src/models.py
+++ src/models.py
@@ -1,10 +1,10 @@
 import numpy as np
 import scipy.integrate as scint
 
-
+# y0 = V0 (or y0 = [K0, V0] for dynamic CC)
 def solutionDE(de):
-    return lambda t, V0: \
-        scint.odeint(de, V0, t)
+    return lambda y0, t: \
+        scint.odeint(de, y0, t)
 
 
 def exponentialDE(alph, beta):
@@ -42,3 +42,32 @@ def generalBertalanffyDE(alph, beta, lamb):
         lambda V, t: \
             alph * V ** lamb - beta * V
     )
+
+
+# Extra models
+
+# https://doi.org/10.1371/journal.pcbi.1003800 (p. 3)
+def exponentialLinearDE(a, b, tau):
+    return solutionDE(
+        lambda V, t: \
+            a * V if t <= tau else b
+    )
+
+def generalLogisticDE(a, v, K):
+    return solutionDE(
+        lambda V, t: \
+            gamm * V * (1 - (V / K) ** v)
+    )
+
+def dynamicCarryingCapacityDE(a, b):
+    def dKdt(V):
+        return b * V ** (2 / 3)
+    def dVdt(K, V):
+        return a * V * np.log(K / V)
+
+    return solutionDE(
+        # y is a [K, V] vector
+        lambda y, t : \
+            [dKdt(y[1]), dVdt(y[0], y[1])]
+    )
+
