fc6564fe748077e8ff3cb779961695c8a257a9f8
vdlluca <luca.iacovella@vub.be>
2023-05-03 23:14:24 +0200

    restructured model parameters

--- src/models.py
+++ src/models.py
@@ -1,73 +1,84 @@
 import numpy as np
 import scipy.integrate as scint
 
+
+
 # y0 = V(t=0) (or y0 = [K(t=0), V(t=0)] for dynamic CC)
 def solutionDE(de):
-    return lambda y0, t: \
+    return lambda t, y0: \
         scint.odeint(de, y0, t)
 
 
-def exponentialDE(alph, beta):
-    return solutionDE(
-        lambda V, t: \
-            (alph - beta) * V
-    )
+# Introduction to mathematical oncology (table 2.1)
+class Exponential:
+    def predict(t, V0, a, b):
+        return solutionDE(
+            lambda V, t: \
+                (a - b) * V
+        )(t, V0)
 
-def logisticDE(gamm, K):
-    return solutionDE(
-        lambda V, t: \
-            gamm * V * (1 - V / K)
-    )
+class LogisticVerhulst:
+    def predict(t, V0, a, b):
+        return solutionDE(
+            lambda V, t: \
+                a * V - b * V ** 2
+        )(t, V0)
 
-def gompertzDE(gamm, delt):
-    return solutionDE(
-        lambda V, t: \
-            V * (delt - gamm * np.log(V))
-    )
+class Gompertz:
+    def predict(t, V0, a, b):
+        return solutionDE(
+            lambda V, t: \
+                V * (b - a * np.log(V))
+        )(t, V0)
 
-def generalGompertzDE(gamm, delt, lamb):
-    return solutionDE(
-        lambda V, t: \
-            V ** lamb * (delt - gamm * np.log(V))
-    )
+class GeneralGompertz:
+    def predict(t, V0, a, b, l):
+        return solutionDE(
+            lambda V, t: \
+                V ** l * (b - a * np.log(V))
+        )(t, V0)
 
-def classicBertalanffyDE(alph, beta):
-    return solutionDE(
-        lambda V, t: \
-            alph * V ** (2 / 3) - beta * V
-    )
+class ClassicBertalanffy:
+    def predict(t, V0, a, b):
+        return solutionDE(
+            lambda V, t: \
+                a * V ** (2 / 3) - b * V
+        )(t, V0)
 
-def generalBertalanffyDE(alph, beta, lamb):
+class GeneralBertalanffy:
+    def predict(t, V0, a, b, l):
     return solutionDE(
         lambda V, t: \
-            alph * V ** lamb - beta * V
-    )
+            a * V ** l - b * V
+    )(t, V0)
 
 
 # Extra models
 # https://doi.org/10.1371/journal.pcbi.1003800 (p. 3)
 
-def exponentialLinearDE(a, b, tau):
-    return solutionDE(
-        lambda V, t: \
-            a * V if t <= tau else b
-    )
+class ExponentialLinear:
+    def predict(t, V0, a, b, u):
+        return solutionDE(
+            lambda V, t: \
+                a * V if t <= u else b
+        )(t, V0)
 
-def generalLogisticDE(a, v, K):
-    return solutionDE(
-        lambda V, t: \
-            gamm * V * (1 - (V / K) ** v)
-    )
+class GeneralLogistic:
+    def predict(t, V0, a, v, K):
+        return solutionDE(
+            lambda V, t: \
+                gamm * V * (1 - (V / K) ** v)
+        )(t, V0)
 
-def dynamicCarryingCapacityDE(a, b):
-    def dKdt(V):
-        return b * V ** (2 / 3)
-    def dVdt(K, V):
-        return a * V * np.log(K / V)
+class dynamicCarryingCapacity:
+    def predict(t, V0, a, b):
+        def dKdt(V):
+            return b * V ** (2 / 3)
+        def dVdt(K, V):
+            return a * V * np.log(K / V)
 
-    return solutionDE(
-        # y is a [K, V] vector
-        lambda y, t : \
-            [dKdt(y[1]), dVdt(y[0], y[1])]
-    )
-
+        return solutionDE(
+            # y is a [K, V] vector
+            lambda y, t : \
+                [dKdt(y[1]), dVdt(y[0], y[1])]
+        )(t, V0)
