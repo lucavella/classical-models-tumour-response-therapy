2103c7c2d6e82aa95b59c5ebe7f01d2d6d478970
vdlluca <luca.iacovella@vub.be>
2023-05-04 15:08:19 +0200

    preprocess returns list instead of merged studies

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -20,25 +20,34 @@ def check_patient_overlap(studies):
 # output: combined preprocessed dataframe
 def preprocess(studies):
     for study in studies:
+        # extract study and arm nr
+        study['StudyNr'] = \
+            study['StudyArm'].apply(lambda saTxt: int(saTxt[6]))
+        study['Arm'] = \
+            study['StudyArm'].apply(lambda saTxt: int(saTxt[-1]))
+        study.drop('StudyArm', axis=1, inplace=True)
+
         # set nonnumeric values to 0
-        study['TargetLesionLongDiam_mm'] = utils.clean_nonnumeric(study['TargetLesionLongDiam_mm'], with_value=0)
+        study['TargetLesionLongDiam_mm'] = \
+            utils.clean_nonnumeric(study['TargetLesionLongDiam_mm'], with_value=0)
         study['TargetLesionLongDiam_mm'].astype('float')
 
         # calculate tumor volume using formula
-        study['TumorVolume_mm3'] = study['TargetLesionLongDiam_mm'].apply(lambda ld: ld ** 3 * 0.5)
-
-    # merge all dataframes
-    studies = pd.concat(studies, ignore_index=True)
+        study['TumorVolume_mm3'] = \
+            study['TargetLesionLongDiam_mm'].apply(lambda ld: ld ** 3 * 0.5)
 
     # normalize tumor volume to range of [0,1]
-    min_tv = studies['TumorVolume_mm3'].min()
-    max_tv = studies['TumorVolume_mm3'].max()
-    studies['TumorVolumeNorm'] = studies['TumorVolume_mm3'].apply(lambda tv: (tv - min_tv) / (max_tv - min_tv))
-
-    # extract study and arm nr
-    studies['StudyNr'] = studies['StudyArm'].apply(lambda saTxt: int(saTxt[6]))
-    studies['Arm'] = studies['StudyArm'].apply(lambda saTxt: int(saTxt[-1]))
-    studies.drop('StudyArm', axis=1, inplace=True)
+    min_tv = min(map(
+        lambda study: study['TumorVolume_mm3'].min(),
+        studies
+    ))
+    max_tv = max(map(
+        lambda study: study['TumorVolume_mm3'].max(),
+        studies
+    ))
+    for study in studies:
+        study['TumorVolumeNorm'] = \
+            study['TumorVolume_mm3'].apply(lambda tv: (tv - min_tv) / (max_tv - min_tv))
 
     return studies
 
